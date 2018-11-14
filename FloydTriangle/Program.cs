403bd0c011fea2b1e5cac625edc27141f89fefd5
Damla Firat <firatdamla93@gmail.com>
2018-11-14 12:30:27 +0300

    first commit

---
+++ FloydTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            floydTriangle(5);
+
+            Console.ReadLine();
+        }
+
+        private static void floydTriangle(int v)
+        {
+            int k = 1;
+
+            for (int i = 1; i <= v; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(k + "\t");
+                    k++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
